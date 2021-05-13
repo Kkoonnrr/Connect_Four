c6daca4b950eca4cdf0ec0eb4220dce171bfe81f
kkoonnrr <konrad@synowiec.eu>
2021-05-13 23:21:04 +0200

    Pygame-szkic

--- Code/Main.py
+++ Code/Main.py
@@ -1,6 +1,15 @@
 import numpy as np
+import pygame
+import sys
+import math
+G = (0,255,0)
+B = (0,0,0)
+R = (255,0,0)
+Y = (255,255,0)
+RADIUS = 45
 ROW_COUNT = 6
 COLUMN_COUNT = 7
+SIZE = 100
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
@@ -41,31 +50,50 @@ def winning_check(board, sign):
             if board[r][c] == sign and board[r+1][c+1] == sign and board[r+2][c+2] == sign and board[r+3][c+3] == sign:
                 return True
 
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, G, (c*SIZE, r*SIZE+SIZE, SIZE, SIZE))
+            if board[r][c] == 0:
+                pygame.draw.circle(screen, B, (c*SIZE+SIZE/2, r*SIZE+SIZE+SIZE/2), RADIUS)
+            elif board[r][c] == 1:
+                pygame.draw.circle(screen, R, (c * SIZE + SIZE / 2, r * SIZE + SIZE + SIZE / 2), RADIUS)
+            else:
+                pygame.draw.circle(screen, Y, (c * SIZE + SIZE / 2, r * SIZE + SIZE + SIZE / 2), RADIUS)
+    pygame.display.update()
 board = create_board()
 print(board)
 game_over = False
 turn = 0
 
+pygame.init()
+width = COLUMN_COUNT * SIZE
+height = COLUMN_COUNT * SIZE
+size = (width, height)
+screen = pygame.display.set_mode(size)
+draw_board(board)
 while not game_over:
-    if turn == 0:
-        col = int(input("Player 1 moves: "))
-        if correct_location(board, col):
-            row = next_free_space(board, col)
-            drop(board, col, row, 1)
-            if winning_check(board, 1):
-                print("Player 1 Wins")
-                game_over = True
-
-    else:
-        col = int(input("Player 2 moves: "))
-        if correct_location(board, col):
-            row = next_free_space(board, col)
-            drop(board, col, row, 2)
-            if winning_check(board, 2):
-                print("Player 2 Wins")
-                game_over = True
-    turn +=1
-    turn = turn%2
-    #print(select)
-    #print(type(select))
-    print(board)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if turn == 0:
+                col = int(math.floor(event.pos[0]/SIZE))
+                if correct_location(board, col):
+                    row = next_free_space(board, col)
+                    drop(board, col, row, 1)
+                    if winning_check(board, 1):
+                        print("Player 1 Wins")
+                        game_over = True
+            else:
+                col = int(math.floor(event.pos[0]/SIZE))
+                if correct_location(board, col):
+                    row = next_free_space(board, col)
+                    drop(board, col, row, 2)
+                    if winning_check(board, 2):
+                        print("Player 2 Wins")
+                        game_over = True
+            turn += 1
+            turn = turn % 2
+            print(board)
+            draw_board(board)
